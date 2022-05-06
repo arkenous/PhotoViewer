66c03f7f192d19ae95920b7d0ad7b9f8ad0468c5
Kensuke Kosaka <k@arkenous.net>
2022-05-06 15:59:32 +0900

    create project

---
+++ PhotoViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoViewer
+//
+//  Created by 髙坂賢佑 on 2022/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoViewer/PhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerApp.swift
+//  PhotoViewer
+//
+//  Created by 髙坂賢佑 on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct PhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
